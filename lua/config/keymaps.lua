3e1660820d409474e5c9c7ce2ec4e6ae6f67d388
Lorenzo Feroleto <lorenzo@fiveelementslabs.com>
2023-07-04 23:05:59 +0200

    chore(keymaps): added from old config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,32 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Silent keymap option
+local opts = { silent = true }
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Scroll down but stay centered
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- Join lines but do not move cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Center screen when found the pattern
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Delete and paste to black hole register
+vim.keymap.set("n", "<leader>d", '"_d')
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
---
+++ lua/plugins/css-colors.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "ap/vim-css-color",
+  },
+}
