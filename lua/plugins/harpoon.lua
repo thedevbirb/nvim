0ac74e18b289e59df6afb8f5dcaaa7eab1a6bdd7
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-05-22 13:40:07 +0200

    feat: harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,46 +1,63 @@
 return {
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    depedencies = { "nvim-lua/plenary.nvim" },
     lazy = true,
-    keys = {
-      {
-        "<leader>a",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        desc = "Add current buffer to harpoon",
-      },
-      {
-        "<C-e>",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        desc = "Toggle harpoon quick menu",
-      },
-      {
-        "<C-h>",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-      },
-      {
-        "<C-j>",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-      },
-      {
-        "<C-k>",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-      },
-      {
-        "<C-l>",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-      },
-    },
+    keys = function()
+      local harpoon = require("harpoon")
+      return {
+        {
+          "<leader>a",
+          function()
+            harpoon:list():add()
+          end,
+          desc = "Add current buffer to harpoon",
+        },
+        {
+          "<C-e>",
+          function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Toggle harpoon quick menu",
+        },
+        {
+          "<C-h>",
+          function()
+            harpoon:list():select(1)
+          end,
+        },
+        {
+          "<C-j>",
+          function()
+            harpoon:list():select(2)
+          end,
+        },
+        {
+          "<C-k>",
+          function()
+            harpoon:list():select(3)
+          end,
+        },
+        {
+          "<C-l>",
+          function()
+            harpoon:list():select(4)
+          end,
+        },
+        -- {
+        --   "<C-S-P>",
+        --   function()
+        --     harpoon:list():prev()
+        --   end,
+        -- },
+        -- {
+        --   "<C-S-N>",
+        --   function()
+        --     harpoon:list():next()
+        --   end,
+        -- },
+      }
+    end,
   },
 }
