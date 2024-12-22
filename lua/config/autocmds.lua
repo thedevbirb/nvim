bfe14824f2430baffd76290bb28ae273f03beaf9
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-12-22 13:22:11 +0100

    feat: markdown tw; starlark lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,15 +10,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
--- vim.api.nvim_create_autocmd("filetype", {
---   pattern = "netrw",
---   desc = "Better mappings for netrw",
---   callback = function()
---     local bind = function(lhs, rhs)
---       vim.keymap.set("n", lhs, rhs, { remap = true, buffer = true })
---     end
---
---     bind("l", "<cr>")
---     bind("h", "-")
---   end,
--- })
+vim.api.nvim_create_autocmd("filetype", {
+  pattern = "markdown",
+  callback = function()
+    vim.api.nvim_set_option_value("textwidth", 80, { scope = "local" })
+  end,
+})
+
+vim.api.nvim_create_autocmd("filetype", {
+  pattern = "starlark",
+  callback = function()
+    vim.lsp.start({
+      name = "starlark-rust",
+      cmd = { "/Users/birb/.local/share/nvim/mason/packages/starlark-rust/bin/starlark", "--lsp" },
+      root_dir = vim.fs.root(0, { ".git", "main.star" }),
+    })
+    vim.lsp.start({
+      name = "kurtosis",
+      cmd = { "/Users/birb/oss/vscode-kurtosis_kurtosis-tech/starlark-lsp/cmd/starlark-lsp/starlark-lsp", "start" },
+      root_dir = vim.fs.root(0, { ".git", "main.star" }),
+    })
+  end,
+})
