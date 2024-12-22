9f8ca86370860e37f26405031942450d1c3f422e
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2024-12-22 13:25:49 +0100

    chore: document rust-analyzer setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,15 +4,15 @@ return {
     opts = {
       -- Disable them by default, toggle them with <leader>uh
       inlay_hints = { enabled = false },
-      setup = {
-        -- According to :h rustaceanvim.mason. To add rust-analyzer use:
-        -- ```shell
-        -- rustup component add rust-analyzer
-        -- ```
-        rust_analyzer = function()
-          return true
-        end,
-      },
+      -- according to rustaceanvim setup, we should disable rust-analyzer setup
+      -- from mason so you can run different versions of rust-analyzer
+      -- depending on the toolchain if a certain version is missing then
+      -- install with rustup <toolchain> component add rust-analyzer
+      -- setup = {
+      --   rust_analyzer = function()
+      --     return true
+      --   end,
+      -- },
     },
   },
 }
