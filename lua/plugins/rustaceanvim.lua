25c6d01099eedf05b3a53efe4c63212afd9e9db2
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-03-03 09:38:55 +0100

    chore(rust): update rustaceanvim with overrides

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -7,60 +7,58 @@ return {
     "mrcjkb/rustaceanvim",
     version = vim.fn.has("nvim-0.10.0") == 0 and "^4" or false,
     ft = { "rust" },
-    opts = function()
-      return {
-        server = {
-          on_attach = function(_, bufnr)
-            vim.keymap.set("n", "<leader>cR", function()
-              vim.cmd.RustLsp("codeAction")
-            end, { desc = "Code Action", buffer = bufnr })
-            vim.keymap.set("n", "<leader>dr", function()
-              vim.cmd.RustLsp("debuggables")
-            end, { desc = "Rust Debuggables", buffer = bufnr })
-          end,
-          default_settings = {
-            -- rust-analyzer language server configuration
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-                loadOutDirsFromCheck = true,
-                buildScripts = {
-                  enable = true,
-                },
-              },
-              -- Add clippy lints for Rust if using rust-analyzer
-              checkOnSave = diagnostics == "rust-analyzer",
-              -- Enable diagnostics if using rust-analyzer
-              diagnostics = {
-                enable = diagnostics == "rust-analyzer",
-              },
-              procMacro = {
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust Debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
                 enable = true,
-                ignored = {
-                  -- thedevbirb: I don't have all these warnings
-                  -- ["async-trait"] = { "async_trait" },
-                  ["napi-derive"] = { "napi" },
-                  ["async-recursion"] = { "async_recursion" },
-                },
               },
-              files = {
-                excludeDirs = {
-                  ".direnv",
-                  ".git",
-                  ".github",
-                  ".gitlab",
-                  "bin",
-                  "node_modules",
-                  "target",
-                  "venv",
-                  ".venv",
-                },
+            },
+            -- Add clippy lints for Rust if using rust-analyzer
+            checkOnSave = diagnostics == "rust-analyzer",
+            -- Enable diagnostics if using rust-analyzer
+            diagnostics = {
+              enable = diagnostics == "rust-analyzer",
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                -- thedevbirb: I don't have all these warnings
+                -- ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+            files = {
+              excludeDirs = {
+                ".direnv",
+                ".git",
+                ".github",
+                ".gitlab",
+                "bin",
+                "node_modules",
+                "target",
+                "venv",
+                ".venv",
               },
             },
           },
         },
-      }
-    end,
+      },
+    },
     config = function(_, opts)
       if LazyVim.has("mason.nvim") then
         local package_path = require("mason-registry").get_package("codelldb"):get_install_path()
