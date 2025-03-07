291a3e36e30913d20f6e8219de834883232c1aae
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-03-07 23:17:15 +0100

    chore(rust): use nightly formatter by default

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -55,6 +55,11 @@ return {
                 ".venv",
               },
             },
+            rustfmt = {
+              extraArgs = {
+                "+nightly",
+              },
+            },
           },
         },
       },
